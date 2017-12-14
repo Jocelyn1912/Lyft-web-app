4c492158287abaa34f5ad25386060a2b8ca9667a
Jocelyn1912 <toty_2410@hotmail.com>
2017-12-14 11:19:46 -0300

    Boton next

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,10 +6,26 @@ $(document).ready(function(){
   *Función jQuery que permite que se ejecute Splash Screen
   */
   $('.splash-screen').fadeOut(4000);
-
   /*
-  *Función número de celular
+  *Función desplazamiento a la siguiente vista
   */
-  
+  $("#netx1").click(function(){
+    $('.auto-lyft').addClass('hidden');
+    $('.numero-telefonico').removeClass('hidden');
+  });
 
+  $("#next2").click(function(){
+    $('.numero-telefonico').addClass('hidden');
+    $('.verify-phone-number').removeClass('hidden');
+  });
+
+  $("#next3").click(function(){
+    $('.verify-phone-number').addClass('hidden');
+    $('.register').removeClass('hidden');
+  });
+
+  $(".next-end").click(function(){
+    $('.register').addClass('hidden');
+    $('.end').removeClass('hidden');
+  });
 });
