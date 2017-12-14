82029e1fa43e608c87d25c90f09e0653ca417eee
Jocelyn1912 <toty_2410@hotmail.com>
2017-12-14 17:07:06 -0300

    Función código LAB, función volver a la vista anterior y arreglos

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,6 +1,7 @@
 /*
 *Función principal
 */
+
 $(document).ready(function(){
   /*
   *Función jQuery que permite que se ejecute Splash Screen
@@ -28,4 +29,36 @@ $(document).ready(function(){
     $('.register').addClass('hidden');
     $('.end').removeClass('hidden');
   });
+  /*
+  *Función desplazamiento a la vista anterior
+  */
+  $("#previous").click(function(){
+    $('.numero-telefonico').addClass('hidden');
+    $('.auto-lyft').removeClass('hidden');
+  });
+
+  $("#previous1").click(function(){
+    $('.verify-phone-number').addClass('hidden');
+    $('.numero-telefonico').removeClass('hidden');
+  });
+
+  $("#previous2").click(function(){
+    $('.register').addClass('hidden');
+    $('.verify-phone-number').removeClass('hidden');
+  });
+  /*
+  *Función que me dá el código LAB
+  */
+  $("#codigo, #codigo-nuevo").click(function(){
+    var codigo = "";
+    var str = "123456789";
+    var frase = "Tu código LAB es "
+    for (var i = 0; i < 3; i++){
+      codigo += str.charAt(Math.floor(Math.random() * str.length));
+    };
+    alert (frase + codigo);
+  });
+
+
 });
+
